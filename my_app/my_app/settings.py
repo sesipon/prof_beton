559b15b0724b8f48981e5d095e61cedd78706d3e
Sergei <108064960+sesipon@users.noreply.github.com>
2024-04-12 16:22:27 +0300

    secret

--- my_app/my_app/settings.py
+++ my_app/my_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from secret import x_code
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#j@^g2z7f80l0b48zi^bs5ji5x-k@jhoj3kw-a3+q*xn13*(a9'
+SECRET_KEY = x_code
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
